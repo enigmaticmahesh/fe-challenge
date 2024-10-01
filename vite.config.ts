5f88e17ef343f8698f857cf4cc3b7f81e418cd4f
enigmaticmahesh <challengermahesh@gmail.com>
2024-10-02 03:54:24 +0530

    legacy compiler warning fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import sass from 'sass';
 
 // https://vitejs.dev/config/
@@ -9,7 +9,8 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         implementation: sass,
+        api: 'modern-compiler',
       },
     },
   },
-})
+});
